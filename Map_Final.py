fc37f25e6ea429863f91d335b64248f9a36fd318
TAKTUK <taktukgg@gmail.com>
2018-06-19 14:56:25 -0400

    Update Map_Final.py

--- Map_Final.py
+++ Map_Final.py
@@ -1,23 +1,27 @@
 from ipywidgets import HTML
-from ipyleaflet import Map, Marker, Popup
+from ipyleaflet import Map, Marker, Popup,basemaps, basemap_to_tiles
 
-#construction of MAP
-map = Map(center = [19.087991, -71.715163] , zoom = 8)
+
+#map
+map = Map(center = [19.087991, -71.715163] , zoom = 9)
 place = [19.097991, -71.715163]
-#construction of MARKER
 marker = Marker( location = place  , draggable = False)
 map.add_layer(marker)
 
-#construction of popUP
+#popup
 message = HTML()
-message.value = "Point of convergence : marketplace: Dajabon"
+message.value = "This is the marketplce Dajabon"
 popup = Popup(
     location = place,
-    close_button = False,
-    keep_in_view = True
+    
+    close_button = True
     
 )
-map.add_layer(popup)
-marker.popup = message
+sample_layer = basemap_to_tiles(basemaps.Stamen.Terrain)
 
+
+
+map.add_layer(popup)
+map.add_layer(sample_layer)
+marker.popup = message
 map
